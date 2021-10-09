fe1fcbb556e3bdcd7f3e16ab70798c8f2d379223
Renato Avelino <43862847+RenatoAvelino@users.noreply.github.com>
2021-10-09 19:25:45 -0300

    Blue and Yellow collors

--- Assets/Scripts/Rastros/Yellow.cs
+++ Assets/Scripts/Rastros/Yellow.cs
@@ -5,21 +5,24 @@ using UnityEngine;
 public class Yellow : MonoBehaviour
 {
     // Start is called before the first frame update
+    [SerializeField]
+    private float Fat = 2;
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.gameObject.tag == "Pedra")
+        if (collision.gameObject.tag == "Pedra")
         {
-            //TODO Atrito 
+            GameObject tmp = collision.gameObject;
+            tmp.GetComponent<Rigidbody2D>().drag = 1 + Fat;
         }
     }
 }
