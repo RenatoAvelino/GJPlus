d03f89c602af5da806966a45715035b7d2110270
Renato Avelino <43862847+RenatoAvelino@users.noreply.github.com>
2021-10-09 15:45:10 -0300

    Pedra rastro base

--- Assets/Scripts/Pedra.cs
+++ Assets/Scripts/Pedra.cs
@@ -9,6 +9,11 @@ public class Pedra : MonoBehaviour
     public enum Tipos { Leve, Medio, Pesado, Prender}
     public Tipos tipo;
     public int _indexOwner; //Player 1 = 0, Player 2 = 1
+    public GameObject _rastro;
+    [SerializeField]
+    private float _paintDelay = .8f;
+    private float _timer = 0;
+    private bool _isPainting = true;
 
     // Start is called before the first frame update
     void Start()
@@ -19,6 +24,24 @@ public class Pedra : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(rb.velocity.magnitude == 0)
+        {
+            _isPainting = false;
+        }
+        if (_isPainting)
+        {
+            _timer += Time.deltaTime;
+            if(_timer >= _paintDelay)
+            {
+                _timer -= _paintDelay;
+                Paint();
+            }
+        }
+    }
 
+    private void Paint()
+    {
+        GameObject tmp = Instantiate(_rastro);
+        tmp.transform.position = this.transform.position;
     }
 }
