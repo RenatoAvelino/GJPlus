a15bb25ee3f7974d568a03a4e27a83e073a679c4
Bruno Carvalho Lima <brunoorvalho@hotmail.com>
2021-10-08 23:26:43 -0300

    Pedra

---
+++ Assets/Scripts/Pedra.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pedra : MonoBehaviour
+{
+    private Rigidbody2D rb;
+
+    public enum Tipos { Leve, Medio, Pesado, Prender}
+    public Tipos tipo;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (rb.velocity.magnitude == 0)
+        {
+            print("Parou");
+        }
+    }
+}
